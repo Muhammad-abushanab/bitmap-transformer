eb4a8df8a034896292c971af6102b25edc48bc76
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-08-15 21:12:11 +0300

    update ReadMe.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # bitmap-transformer
+
+## What is bitmap-transformer
+
+a project that reads a bmp image file and modify the image in more than one method
+
+- ReverseBlackAndGreen : a method to convert all black color to green and the white color to black
+- flipVertically : a method that flips the image upside down.
+- random : a method that changes the color of the image randomly.
+
+
+## How it works
+
+- clone the repo and cd into `bitmap-transformer`
+- on your terminal run `gradle run --args "inputFile outputFile transfromMethod"`.
+    - Where the `inputfile is : the absloute path for your bmp image`
+    - `outputFile : is the name of your modified image`
+    -`transformerMethod : on of the method explained at the first of this readme file`
+    - example `gradle run --args "D:\muhammad\test.bmp modified.bmp reverseblackandgreen"`
+
+## Contributors
+1. Mohammad Abushanab
+2. Omar Al Issawi
--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -15,49 +15,57 @@ public class App {
 
     public static void main(String[] args) {
         try {
-            String inputFile = args[0];
-            String directoryPath = new File(inputFile).getParent() + "\\" + args[1];
-            Bitmap bitmap = new Bitmap(inputFile,directoryPath);
+            if (args.length < 3) {
+                System.err.println("usage gradle run --args inputFile outputfile transform");
+            } else {
+                String inputFile = args[0];
+                String directoryPath = new File(inputFile).getParent() + "\\" + args[1];
+                Bitmap bitmap = new Bitmap(inputFile, directoryPath);
 
-
-            switch (args[2].toLowerCase()) {
-                case "random" : {
-                    bitmap.random();
-                }break;
-                case "reverseblackandgreen" : {
-                    bitmap.reverseBlackAndGreen();
-                }break;
-                case "flipvertically": {
-                    bitmap.flipVertically();
-                }break;
-                case "stretchvertically": {
-                    bitmap.stretchVertically();
-                }break;
-                default: System.err.println("Invalid transform type");
+                switch (args[2].toLowerCase()) {
+                    case "random": {
+                        bitmap.random();
+                    }
+                        break;
+                    case "reverseblackandgreen": {
+                        bitmap.reverseBlackAndGreen();
+                    }
+                        break;
+                    case "flipvertically": {
+                        bitmap.flipVertically();
+                    }
+                        break;
+                    case "stretchvertically": {
+                        bitmap.stretchVertically();
+                    }
+                        break;
+                    default:
+                        System.err.println("Invalid transform type");
+                }
+                bitmap.save(directoryPath);
             }
-            bitmap.save(directoryPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    }
-//    public  void seeBMPImage(String BMPFileName) throws IOException {
-//        BufferedImage image = ImageIO.read(new File(BMPFileName));
+}
+// public void seeBMPImage(String BMPFileName) throws IOException {
+// BufferedImage image = ImageIO.read(new File(BMPFileName));
 //
-//        int[][] array2D = new int[image.getWidth()][image.getHeight()];
+// int[][] array2D = new int[image.getWidth()][image.getHeight()];
 //
-//        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++)
-//        {
-//            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++)
-//            {
-//                int color = image.getRGB(xPixel, yPixel);
-//                if (color== Color.BLACK.getRGB()) {
-//                    array2D[xPixel][yPixel] = 1;
-//                } else {
-//                    array2D[xPixel][yPixel] = 0; // ?
-//                }
-//            }
-//        }
-//    } }
+// for (int xPixel = 0; xPixel < image.getWidth(); xPixel++)
+// {
+// for (int yPixel = 0; yPixel < image.getHeight(); yPixel++)
+// {
+// int color = image.getRGB(xPixel, yPixel);
+// if (color== Color.BLACK.getRGB()) {
+// array2D[xPixel][yPixel] = 1;
+// } else {
+// array2D[xPixel][yPixel] = 0; // ?
+// }
+// }
+// }
+// } }
